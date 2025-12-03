fb8ee28b377a8adb7d4ecfea2e9a48f1cb0f95c7
Ridwan2024-P <ahsanrid@gmail.com>
2025-12-03 09:52:31 +0600

    fix session

--- logout.php
+++ logout.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
+
+
+
+
+$_SESSION = [];
+session_unset();
 session_destroy();
+setcookie(session_name(), '', time() - 3600, '/');
+
 header("Location: index.php");
-exit;
+exit();
 ?>
